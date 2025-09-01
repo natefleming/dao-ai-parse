40ed537c5a83dcb819dad59ad4c9d2be623d4111
natefleming <natefleming>
2025-09-01 15:08:07 +0000

    refactor to run as notebook

--- notebooks/01_parse_documents.py
+++ notebooks/01_parse_documents.py
@@ -1,111 +1,106 @@
 # Databricks notebook source
-# MAGIC %pip install --upgrade transformers pypdf langchain-text-splitters databricks-vectorsearch mlflow tiktoken torch llama-index docling
-# MAGIC dbutils.library.restartPython()
+from typing import Sequence
+
+pip_requirements: Sequence[str] = (
+  "dao-ai",
+  "databricks-vectorsearch",
+  "docling",
+  "langchain-text-splitters",
+  "llama-index",
+  "mlflow",
+  "pypdf",
+  "tiktoken",
+  "torch",
+  "transformers"
+)
+
+pip_requirements: str = " ".join(pip_requirements)
+
+%pip install --quiet --upgrade {pip_requirements}
+%pip uninstall --quiet -y databricks-connect pyspark pyspark-connect
+%pip install --quiet databricks-connect
+dbutils.library.restartPython()
 
 # COMMAND ----------
 
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
+from importlib.metadata import version
 
-# COMMAND ----------
-
-from typing import Any, Dict, Optional, List
-
-from mlflow.models import ModelConfig
-
-
-model_config_file: str = "model_config.yaml"
-model_config: ModelConfig = ModelConfig(development_config=model_config_file)
-
-databricks_resources: Dict[str, Any] = model_config.get("databricks_resources")
-retriever_config: Dict[str, Any] = model_config.get("retriever_config")
-schema: Dict[str, Any] = retriever_config.get("schema")
-
-
-catalog_name: str = databricks_resources.get("catalog_name")
-database_name: str = databricks_resources.get("database_name")
-volume_name: str = databricks_resources.get("volume_name")
-users: List[str] = databricks_resources.get("users")
-source_table_name: str = retriever_config.get("source_table_name")
-vector_search_index: str = retriever_config.get("vector_search_index")
-vector_search_endpoint_name: str = databricks_resources.get(
-    "vector_search_endpoint_name"
-)
-primary_key: str = schema.get("primary_key")
-embedding_source_column: str = schema.get("embedding_source_column")
-embedding_model_endpoint_name: str = databricks_resources.get(
-    "embedding_model_endpoint_name"
-)
-
-print(f"{catalog_name=}")
-print(f"{database_name=}")
-print(f"{volume_name=}")
-print(f"{source_table_name=}")
-print(f"{vector_search_endpoint_name=}")
-print(f"{vector_search_index=}")
-print(f"{primary_key=}")
-print(f"{embedding_source_column=}")
-print(f"{embedding_model_endpoint_name=}")
-print(f"{users=}")
-
-# COMMAND ----------
-
-from databricks.sdk import WorkspaceClient
-from databricks.sdk.service.catalog import (
-  CatalogInfo, 
-  SchemaInfo, 
-  VolumeInfo, 
-  VolumeType,
-  SecurableType,
-  PermissionsChange,
-  Privilege
-)
-
-import app.catalog
-
-w: WorkspaceClient = WorkspaceClient()
-
-catalog: CatalogInfo 
-try:
-  catalog = w.catalogs.get(catalog_name)
-except Exception as e: 
-  catalog = w.catalogs.create(catalog_name)
-
-schema: SchemaInfo
-try:
-  schema = w.schemas.get(f"{catalog.full_name}.{database_name}")
-except Exception as e:
-  schema = w.schemas.create(database_name, catalog.full_name)
-  
-volume: VolumeInfo
-try:
-  volume = w.volumes.read(f"{catalog.full_name}.{database_name}.{volume_name}")
-except Exception as e:
-  volume = w.volumes.create(catalog.full_name, schema.name, volume_name, VolumeType.MANAGED)
-
-for user in users:
-  user: str
-  w.grants.update(
-    full_name=catalog.full_name,
-    securable_type=SecurableType.CATALOG,
-    changes=[
-      PermissionsChange(add=[Privilege.ALL_PRIVILEGES], principal=user)
-    ])
-  
-spark.sql(f"USE {catalog.name}.{database_name}")
+print(f"dao-ai=={version('dao-ai')}")
+print(f"databricks-vectorsearch=={version('databricks-vectorsearch')}")
+print(f"docling=={version('docling')}")
+print(f"langchain=={version('langchain')}")
+print(f"llama-index=={version('llama-index')}")
+print(f"mlflow=={version('mlflow')}")
+print(f"pypdf=={version('pypdf')}")
+print(f"tiktoken=={version('tiktoken')}")
+print(f"torch=={version('torch')}")
+print(f"transformers=={version('transformers')}")
 
 # COMMAND ----------
 
 from pathlib import Path
 
-raw_documents_path: Path = volume.as_path() / "sample_documents"
-raw_checkpoint_path: Path = volume.as_path() / "checkpoints/raw_docs"
 
-w.files.create_directory(raw_documents_path)
+dbutils.widgets.text(name="config-path", defaultValue="../config/example.yaml")
+config_path: str = dbutils.widgets.get("config-path")
+
+dbutils.widgets.text(name="vector-store-alias", defaultValue="documents_vector_store")
+vector_store_alias: str = dbutils.widgets.get("vector-store-alias")
+
+if not Path(config_path).exists():
+  raise ValueError(f"Config file not found: {config_path}")
+
+print(f"{config_path=}")
+print(f"{vector_store_alias=}")
+
+# COMMAND ----------
+
+from dao_ai.config import AppConfig
+
+
+app: AppConfig = AppConfig.from_file(config_path)
+
+# COMMAND ----------
+
+from dao_ai.config import SchemaModel
+
+for _, schema_model in app.schemas.items():
+  schema_model: SchemaModel
+  schema_model.create()
+
+
+# COMMAND ----------
+
+from dao_ai.config import VectorStoreModel
+
+if vector_store_alias not in app.resources.vector_stores:
+  raise ValueError(f"Vector store not found: {vector_store_alias}")
+
+vector_store_model: VectorStoreModel = app.resources.vector_stores[vector_store_alias]
+
+print(vector_store_model)
+
+# COMMAND ----------
+
+from pathlib import Path
+from dao_ai.config import VolumePathModel
+
+source_path_model: VolumePathModel = vector_store_model.source_path
+source_path_model.create()
+
+checkpoints_path_model: VolumePathModel = vector_store_model.checkpoint_path
+checkpoints_path_model.create()
+
+source_documents_path: Path = Path(source_path_model.full_name)
+source_checkpoint_path: Path = Path(checkpoints_path_model.full_name)
+
+print(source_documents_path)
+print(source_checkpoint_path)
 
 # COMMAND ----------
 
 
+source_table_name: str = vector_store_model.source_table.full_name
 document_uri: str = "source"
 
 spark.sql(f"""
@@ -119,36 +114,32 @@ spark.sql(f"""
 
 # COMMAND ----------
 
-from pyspark.sql import DataFrame
+from typing import Iterator
+from io import BytesIO
+import warnings
+import mimetypes
+import json
 
+from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
-from app.udf import guess_mime_type
-
-
-df: DataFrame = (
-    spark.readStream.format("cloudFiles")
-    .option("cloudFiles.format", "BINARYFILE")
-    .load(raw_documents_path.as_posix())
+from docling.document_converter import DocumentConverter, ConversionResult
+from docling.datamodel.base_models import InputFormat, DocumentStream
+from docling.document_converter import (
+    DocumentConverter,
+    PdfFormatOption,
+    WordFormatOption,
 )
+from docling.pipeline.simple_pipeline import SimplePipeline
+from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
+from docling_core.types.doc.document import DoclingDocument
 
-df = df.withColumn("mime_type", guess_mime_type(df.path))
-
-(
-    df.writeStream.trigger(availableNow=True)
-    .option("checkpointLocation", raw_checkpoint_path)
-    .table(f"{catalog_name}.{database_name}.raw_docs")
-    .awaitTermination()
-)
-
-# COMMAND ----------
-
-display(spark.table(f"{catalog_name}.{database_name}.raw_docs"))
-
-# COMMAND ----------
-
-from typing import Iterator
+pipeline_options: PdfPipelineOptions = PdfPipelineOptions()
+pipeline_options.do_ocr = False
+pipeline_options.do_table_structure = True
 
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import Document, set_global_tokenizer
@@ -157,12 +148,51 @@ from transformers import AutoTokenizer
 
 import pandas as pd
 
-import pyspark.sql.functions as F
 
-from app.udf import parse_bytes
+## Custom options are now defined per format.
+def document_converter() -> DocumentConverter:
+    converter: DocumentConverter = (
+        DocumentConverter(  # all of the below is optional, has internal defaults.
+            allowed_formats=[
+                InputFormat.PDF,
+                InputFormat.IMAGE,
+                InputFormat.DOCX,
+                InputFormat.HTML,
+                InputFormat.PPTX,
+            ],  # whitelist formats, non-matching files are ignored.
+            format_options={
+                InputFormat.PDF: PdfFormatOption(
+                    pipeline_options=pipeline_options, # pipeline options go here.
+                    backend=PyPdfiumDocumentBackend # optional: pick an alternative backend
+                ),
+                InputFormat.DOCX: WordFormatOption(
+                    pipeline_cls=SimplePipeline # default for office formats and HTML
+                ),
+            },
+        )
+    )
+    return converter
 
-# Reduce the arrow batch size as our PDF can be big in memory
-spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", 10)
+
+def parse_bytes(raw_doc_contents_bytes: bytes) -> str:
+    try:
+      stream: BytesIO = BytesIO(raw_doc_contents_bytes)
+      
+      document_stream: DocumentStream = DocumentStream(name="foo", stream=stream)
+      converter: DocumentConverter = document_converter()
+      result: ConversionResult = converter.convert(document_stream)
+      document: DoclingDocument = result.document
+      markdown: str = document.export_to_markdown()
+      return markdown
+    except Exception as e:
+        warnings.warn(f"Exception {e} has been thrown during parsing")
+        return None
+
+
+@F.udf(T.StringType())
+def guess_mime_type(path: str) -> str:
+    return mimetypes.guess_type(path)[0]
+  
 
 @F.pandas_udf("array<string>")
 def read_as_chunk(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
@@ -171,7 +201,7 @@ def read_as_chunk(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
         AutoTokenizer.from_pretrained("hf-internal-testing/llama-tokenizer")
     )
     # Sentence splitter from llama_index to split on sentences
-    splitter = SentenceSplitter(chunk_size=500, chunk_overlap=10)
+    splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=128)
 
     def extract_and_split(b):
         txt = parse_bytes(b)
@@ -182,24 +212,32 @@ def read_as_chunk(batch_iter: Iterator[pd.Series]) -> Iterator[pd.Series]:
 
     for x in batch_iter:
         yield x.apply(extract_and_split)
+  
 
 # COMMAND ----------
 
-source_table_checkpoint_path: Path = volume.as_path() / "checkpoints/document_chunked"
-print(source_table_checkpoint_path)
+from pyspark.sql import DataFrame
 
-# COMMAND ----------
+
+spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", 10)
 
 document_uri: str = "source"
 
+df: DataFrame = (
+    spark.readStream.format("cloudFiles")
+    .option("cloudFiles.format", "BINARYFILE")
+    .load(source_documents_path.as_posix())
+)
+
 (
-    spark.readStream.table("raw_docs")
-        .withColumn("content", F.explode(read_as_chunk("content")))
-        .selectExpr(f"path as {document_uri}", "content")
-        .writeStream.trigger(availableNow=True)
-        .option("checkpointLocation", source_table_checkpoint_path)
-        .table(source_table_name)
-        .awaitTermination()
+    df
+    .withColumn("mime_type", guess_mime_type(df.path))
+    .withColumn("content", F.explode(read_as_chunk("content")))
+    .selectExpr(f"path as {document_uri}", "mime_type", "content")
+    .writeStream.trigger(availableNow=True)
+    .option("checkpointLocation", source_checkpoint_path.as_posix())
+    .table(source_table_name)
+    .awaitTermination()
 )
 
 # COMMAND ----------
